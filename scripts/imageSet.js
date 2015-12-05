5aa2469a8b68815d44c641948a1f252a914f2278
Kevin Buhrer <kbuhrer@kpmg.com>
2015-12-05 13:51:36 -0700

    Image Set work for adjectives and nouns, Background images, various other improvements

---
+++ scripts/imageSet.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  $('.image-set').each(function() {
+    var $set = $(this);
+    var $full = $set.find('.image-set-full');
+    var $thumbs = $set.find('.image-set-thumbnails').children('.thumbnail');
+
+    var $empty = $('<div/>')
+                  .appendTo($full)
+                  .addClass('image-set-full-content')
+                  .addClass('empty')
+                  .text('Select an image above for a larger view.');
+
+    //$full.append($empty);
+
+    $thumbs.each(function(i, el) {
+      var $thumb = $(this);
+
+      $thumb.on('click', function() {
+        if ($empty) {
+          $empty.remove();
+        }
+
+        var src = $thumb.data('src');
+        $thumbs.removeClass('active');
+        $thumb.addClass('active');
+
+        // Fade out old
+        var $old = $full.find('.image-set-full-content').remove();
+
+        // Fade in new
+        var $new = $('<div/>')
+                    .addClass('image-set-full-content')
+                    .css('background-image', 'url(' + src + ')')
+                    .appendTo($full);
+
+        // Attempt to wait for another rendering pass so fade-in transition works.
+        setTimeout(function(argument) {
+          $new.addClass('in');
+        }, 10);
+      });
+    });
+  });
+});
